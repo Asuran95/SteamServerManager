aec3acc677312e165cc8b8d035334d93b3206ffc
matkijahenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2022-09-29 13:48:19 -0300

    fix: add sub commands to update command, delete UpdateAllServersCommand.java and UpdateGameServersCommand.java

--- src/main/java/steamservermanager/discordbot/commands/update/UpdateServerCommand.java
+++ src/main/java/steamservermanager/discordbot/commands/update/UpdateServerCommand.java
@@ -1,5 +1,7 @@
 package steamservermanager.discordbot.commands.update;
 
+import java.util.List;
+
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import steamservermanager.discordbot.commands.DiscordCommandHandler;
 import steamservermanager.eao.ServerGameEAO;
@@ -17,25 +19,70 @@ public class UpdateServerCommand extends DiscordCommandHandler {
 
 	@Override
 	protected void action(SlashCommandInteractionEvent event) {
-		long id = event.getOption("serverid").getAsLong();
+
+		String subcommandName = event.getSubcommandName();
 		
-		updateServerById(event, id);
-	}
-	
-	private void updateServerById(SlashCommandInteractionEvent event, Long id) {
-		ServerGame serverGame = serverGameEAO.find(id);
-		
-		if (serverGame == null) {
-			//throw new RuntimeException("There is no game server with this ID.");
-			event.reply("There is no game server with this ID.").setEphemeral(true).queue();
+		if (subcommandName.equals("all")) {
+			
+			updateAllServers(event);
+			
+			return;
+			
+		} else if (subcommandName.equals("game")) {
+			
+			int steamId = event.getOption("name").getAsInt();
+			updateServersbySteamID(event, steamId);
+			
+			return;
+			
+		} else if (subcommandName.equals("server")) {
+			
+			long id = event.getOption("name").getAsLong();
+			updateServerById(event, id);
+			
+			return;
+			
 		}
 		
+	}
+
+	private void updateAllServers(SlashCommandInteractionEvent event) {
+		List<ServerGame> serverGameList = serverGameEAO.findAll();
+
+		if (!serverGameList.isEmpty()) {
+			for (ServerGame serverGame : serverGameList) {
+				updateServerGame(event, serverGame);
+			}
+		} else {
+			throw new RuntimeException("There is no game server installed.");
+		}
+	}
+
+	private void updateServersbySteamID(SlashCommandInteractionEvent event, Integer steamId) {
+		List<ServerGame> serverGameList = serverGameEAO.findByAppID(steamId);
+
+		if (!serverGameList.isEmpty()) {
+			for (ServerGame serverGame : serverGameList) {
+				updateServerGame(event, serverGame);
+			}
+		} else {
+			throw new RuntimeException("There is no game server installed with this SteamID.");
+		}
+	}
+
+	private void updateServerById(SlashCommandInteractionEvent event, Long id) {
+		ServerGame serverGame = serverGameEAO.find(id);
+
+		if (serverGame == null) {
+			throw new RuntimeException("There is no game server with this ID.");
+		}
+
 		updateServerGame(event, serverGame);
 	}
-	
+
 	private void updateServerGame(SlashCommandInteractionEvent event, ServerGame serverGame) {
 		createDiscordUpdaterListener(event, serverGame);
-		
+
 		UpdaterServerGameService updaterServerGameService = ServiceProvider.provide(UpdaterServerGameService.class);
 		updaterServerGameService.update(serverGame);
 	}
@@ -43,7 +90,7 @@ public class UpdateServerCommand extends DiscordCommandHandler {
 	private void createDiscordUpdaterListener(SlashCommandInteractionEvent event, ServerGame serverGame) {
 		new UpdateServerListenerAdapter(event, serverGame);
 	}
-	
+
 	@Override
 	public String help() {
 		return "'all' - Update all servers.";
--- src/main/java/steamservermanager/services/DiscordBotService.java
+++ src/main/java/steamservermanager/services/DiscordBotService.java
@@ -1,16 +1,24 @@
 package steamservermanager.services;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDA.Status;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import steamservermanager.discordbot.SteamServerManagerDiscordBot;
 import steamservermanager.discordbot.listener.DiscordBotListener;
 import steamservermanager.eao.DiscordBotEAO;
+import steamservermanager.eao.ServerGameEAO;
 import steamservermanager.events.EventManagerService;
 import steamservermanager.models.DiscordBot;
+import steamservermanager.models.ServerGame;
 import steamservermanager.utils.ServiceProvider;
 
 public class DiscordBotService {
@@ -18,6 +26,7 @@ public class DiscordBotService {
 	private JDA jda;
 	private DiscordBotEAO discordBotEAO = ServiceProvider.provide(DiscordBotEAO.class);
 	private EventManagerService eventManager = ServiceProvider.provide(EventManagerService.class);
+	private ServerGameEAO serverGameEAO = ServiceProvider.provide(ServerGameEAO.class);
 	
 	public void setDiscordBot(DiscordBot discordBot) {
 		DiscordBot discordBotLoaded = discordBotEAO.find(1L);
@@ -62,23 +71,14 @@ public class DiscordBotService {
 	
 	private void restartSlashCommands() {
 		CommandListUpdateAction commands = jda.updateCommands();
+		
+		OptionData subcommandGameOption = 
+				populateGameOption(new OptionData(OptionType.INTEGER, "name", "Game's name", true));
+		OptionData subcommandServerOption = 
+				populateServerOption(new OptionData(OptionType.INTEGER, "name", "Server's name", true));
 
 		commands.addCommands(
 			Commands.slash("serverlist", "Shows the list of available servers.")
-		);
-		
-		commands.addCommands(
-			Commands.slash("updateall", "Updates all listed servers.")
-		);
-		
-		commands.addCommands(
-			Commands.slash("updategame", "Updates all servers from a specific game.")
-				.addOption(OptionType.INTEGER, "steamid", "SteamID of the game.", true)
-		);
-		
-		commands.addCommands(
-			Commands.slash("updateserver", "Updates a specific server.")
-				.addOption(OptionType.INTEGER, "serverid", "ServerID from the list.", true)
 		);
 		
 		commands.addCommands(
@@ -86,7 +86,44 @@ public class DiscordBotService {
 				.addOption(OptionType.INTEGER, "id", "The server's ID.", true)
 		);
 		
+		commands.addCommands(Commands.slash("update", "Update all servers, all servers of a specific game or a specific server")
+				.addSubcommands(new SubcommandData("all", "Updates every server from the list."))
+				.addSubcommands(new SubcommandData("game", "Updates all games from it's Steam ID")
+						.addOptions(subcommandGameOption)
+				)
+				.addSubcommands(new SubcommandData("server", "Updates a specific server")
+						.addOptions(subcommandServerOption)
+				)
+		);
+		
 		commands.queue();
+	}
+	
+	private OptionData populateGameOption(OptionData option) {
+		List<ServerGame> serverList = serverGameEAO.findAll();
+		List<Integer> ids = new ArrayList<Integer>();
+		
+		for (ServerGame serverGame : serverList) {
+			
+			if (!ids.contains(serverGame.getAppID())) {
+				option.addChoice(serverGame.getGameName(), serverGame.getAppID());
+			}
+			
+			ids.add(serverGame.getAppID());
+			
+		}
+		
+		return option;
+	}
+	
+	private OptionData populateServerOption(OptionData option) {
+		List<ServerGame> serverList = serverGameEAO.findAll();
+		
+		for (ServerGame serverGame : serverList) {
+			option.addChoice(serverGame.getServerName(), serverGame.getIdServerGame());
+		}
+		
+		return option;
 	}
 	
 	public Status getStatus() {
