06de5afffc0b12dcddf107b7d2164251e44cec31
Asuran95 <alpgts250@hotmail.com>
2024-09-29 02:30:14 -0300

    Change steamcmd location to library folder instead of tmp

--- src/main/java/steamcmd/SteamCMDBuilder.java
+++ src/main/java/steamcmd/SteamCMDBuilder.java
@@ -3,6 +3,8 @@ package steamcmd;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -33,12 +35,11 @@ public class SteamCMDBuilder {
 	}
 	
 	public SteamCMD build() {
-		
 		if (this.localDir == null) {
-			this.localDir = System.getProperty("java.io.tmpdir") + File.separator;
+			this.localDir = System.getProperty("java.io.tmpdir");
 		}
 		
-		this.localDir = this.localDir + "steamcmd" + File.separator;
+		this.localDir = this.localDir + File.separator + "steamcmd" + File.separator;
 		
 		String steamCmdLocal = localDir;
 		String steamcmdURL = "";
@@ -111,16 +112,16 @@ public class SteamCMDBuilder {
 	private File downloadSteamCMD(String steamcmdURL, File zipFile) {
 		URL url;
 		try {
-			url = new URL(steamcmdURL);
+			url = new URI(steamcmdURL).toURL();
 			ReadableByteChannel rbc = Channels.newChannel(url.openStream());
 			FileOutputStream fos = new FileOutputStream(zipFile);
 			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 			fos.close();
 			rbc.close();
-		} catch (IOException e) {
+			
+		} catch (IOException | URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
-		
 		return zipFile;
 	}
 	
--- src/main/java/steamservermanager/services/UpdaterServerGameService.java
+++ src/main/java/steamservermanager/services/UpdaterServerGameService.java
@@ -3,37 +3,34 @@ package steamservermanager.services;
 import steamcmd.SteamCMD;
 import steamcmd.SteamCMDBuilder;
 import steamservermanager.events.EventManagerService;
+import steamservermanager.models.ManagerSettings;
 import steamservermanager.models.ServerGame;
 import steamservermanager.updaterservergame.UpdaterServerGame;
 import steamservermanager.utils.ServiceProvider;
 
 public class UpdaterServerGameService {
-
 	private UpdaterServerGame updater;
 	private EventManagerService eventManager = ServiceProvider.provide(EventManagerService.class);
 	private ServerRunnerService serverRunnerService = ServiceProvider.provide(ServerRunnerService.class);
-	
-	public UpdaterServerGameService() {
-		setupUpdater();
-	}
 
 	public void update(ServerGame serverGame) {
-		
 		if (updater == null) {
-			setupUpdater();
+			ManagerSettings managerSettings = serverGame.getManagerSettings();
+
+			setupUpdater(managerSettings.getLocalLibrary());
 		}
 		serverRunnerService.stopServer(serverGame);
 		updater.addUpdate(serverGame);
 	}
-	
-	private void setupUpdater() {
-		SteamCMDBuilder builder = 
-        		new SteamCMDBuilder().addListener(eventManager.getSteamCMDListener());
-		
-		SteamCMD steamCmd = builder.build();
-		
+
+	private void setupUpdater(String localLibrary) {
+		SteamCMD steamCmd = new SteamCMDBuilder()
+				.setLocalDir(localLibrary)
+				.addListener(eventManager.getSteamCMDListener())
+				.build();
+
 		updater = new UpdaterServerGame(eventManager.getUpdateMonitorListener(), steamCmd);
-		
+
 		updater.startUpdater();
 	}
 }
